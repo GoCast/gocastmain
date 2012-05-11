9253e011ca1f4591b88d8c7325ae47254d437f0c
Robert Wolff <rwolff@gocast.it>
2012-05-10 17:36:01 -0700

    Beginning of functionality. Login to FB, anon login to xmpp server @ gocast & get friends list and send ID lookup for online status to server on 'make/add to call' button. No response yet. Need to change to IQ from MSG.

--- projects/server/people/people.js
+++ projects/server/people/people.js
@@ -1,12 +1,36 @@
+gSwitchboard = 'switchboard_gocastfriends@video.gocast.it';
 
 $(document).ready(function () {
+	 $('#add_button').click(function () {
+		 var sel =  $("#fbfriends option:selected");
+
+		 if (sel !== "")
+		 {
+			 $('#participant-list').empty();
+	    	 alert("Ready to lookup name:" + $(sel).text() + " ; id:" + $(sel).attr('fbid'));
+	    	 var msg = $msg({to: gSwitchboard, type: 'chat'})
+	    	 	.c('body').t('FB_LOOKUP_JID ; ' + $(sel).attr('fbid'));
+
+	    	 Callcast.connection.send(msg);
+		 }
+
+	 });
 
 });
 
 $(document).bind('connected', function () {
-    $('.button').removeAttr('disabled');
+	console.log("Connected.");
 
-    $('#myjid').append(Peek.connection.jid);
+    $('.button').removeAttr('disabled');
+    $('#myjid').append(Callcast.connection.jid);
+
+	var pres = $pres({to: 'switchboard_gocastfriends@video.gocast.it', intro_sr: globalFBSR})
+		.c('x',{xmlns: 'http://jabber.org/protocol/muc'});
+
+    // Now that we're connected, let's send our presence info to the switchboard and FB info.
+    // Note - we're going to send our INTRO_SR buried in our presence.
+    //        This way, the switchboard will know who we are on facebook when our presence is seen.
+	Callcast.connection.send(pres);
 
     // Handle inbound signaling messages
 });
@@ -41,14 +65,47 @@ function pluginLoaded() {
 };
 
 $(document).bind('FacebookConnected', function(ev, data) {
-	console.log("FacebookConnected bind: ", data);
+	console.log("FacebookConnected. Going for XMPP connection.");
 
-	/* Connect anonymous. */
+	globalFBSR = data.signedRequest;
+	globalFBID = data.id;
+
+	// Need to figure out our name so we can set a nickname.
+	FB.api('/me', function(me){
+	  if (me.name) {
+	  	console.log("On Facebook, I'm known as: " + me.name);
+		Callcast.SetNickname(me.name);
+	  }
+	});
+
+	FB.api("/me/friends?fields=name,picture,id", function handleFriends(response) {
+//        var divInfo = document.getElementById("friendstest");
+
+        var friends = response.data;
+
+//        divInfo.innerHTML += "<h1>My Friends</h1>";
+		if (friends.length > 0)
+		{
+			 $('#fbfriends select').empty();
+			 $('#fbfriends select').removeAttr('disabled');
+		}
+
+        for (var i = 0; i < friends.length; i++) {
+//            divInfo.innerHTML += friends[i].name + "<br />";
+//            divInfo.innerHTML += "<img src=\"" + friends[i].picture + "\" /><br /><br />";
+			 var optionline = '<option fbid=' + friends[i].id;
+
+			 optionline += '>' + friends[i].name + '</option>';
+
+			 $('#fbfriends select').append(optionline);
+		}
+    });
+
+    	/* Connect anonymous. */
 	Callcast.connect(Callcast.CALLCAST_XMPPSERVER, "");
 });
 
 $(window).bind('beforeunload', function() {
-	alert("bind beforeunload");
 	Callcast.disconnect();
 });
 
