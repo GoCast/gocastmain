ef39a71bbe223a0fdd8d7c87b88b9b43b39c07a7
Manjesh Malavalli <mmalavalli@xvdth.com>
2012-07-25 10:43:53 -0700

    Added gyp_chromium command to generate merg_libs project.

--- projects/desktop/gocastplayer/prepdeps.sh
+++ projects/desktop/gocastplayer/prepdeps.sh
@@ -2,7 +2,12 @@
 
 if [ `uname` = "Darwin" ]; then
     echo "Building (Webrtc+Libjingle) monolithic library..."
-    cd deps/webrtc/trunk/src/build
+    cd deps/webrtc/trunk
+
+    echo "build/gyp_chromium --depth=. src/build/merge_libs.gyp"
+    build/gyp_chromium --depth=. src/build/merge_libs.gyp"
+    cd src/build
+
     echo "xcodebuild -project merge_libs.xcodeproj -target merged_lib -configuration $1 -sdk macosx10.6 GCC_ENABLE_CPP_RTTI=YES"
     xcodebuild -project merge_libs.xcodeproj -target merged_lib -configuration $1 -sdk macosx10.6 GCC_ENABLE_CPP_RTTI=YES
     cd ../../../../..
