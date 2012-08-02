67fe6a37b776dd184d46ea9b687f24b2bf7faa18
Manjesh Malavalli <mmalavalli@xvdth.com>
2012-08-02 12:28:13 -0700

    Added scoped lock for accessing 'effect_' member in VideoCapturer class on Mac

--- projects/desktop/gocastplayer/prepdeps.sh
+++ projects/desktop/gocastplayer/prepdeps.sh
@@ -8,8 +8,8 @@ if [ `uname` = "Darwin" ]; then
     build/gyp_chromium --depth=. src/build/merge_libs.gyp
     cd src/build
 
-    echo "xcodebuild -project merge_libs.xcodeproj -target merged_lib -configuration $1 -sdk macosx10.6 GCC_ENABLE_CPP_RTTI=YES"
-    xcodebuild -project merge_libs.xcodeproj -target merged_lib -configuration $1 -sdk macosx10.6 GCC_ENABLE_CPP_RTTI=YES
+    echo "xcodebuild -project merge_libs.xcodeproj -target merged_lib -configuration $1 -arch i386 -sdk macosx10.6 GCC_ENABLE_CPP_RTTI=YES"
+    xcodebuild -project merge_libs.xcodeproj -target merged_lib -configuration $1 -arch i386 -sdk macosx10.6 GCC_ENABLE_CPP_RTTI=YES
     cd ../../../../..
 fi
 
