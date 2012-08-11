7192506c400a2150ab14c7ae45eaa96e17be305b
jimkoning <jimkoning@gmail.com>
2012-08-11 16:23:08 -0700

    factored out chat message utility

---
+++ projects/gocastjs/ui/uiutil.js
@@ -0,0 +1,60 @@
+///
+/// ui util
+///
+
+"use strict";
+
+var GoCastJS = ('undefined' !== typeof(GoCastJS)) ? GoCastJS : {};
+GoCastJS = (null !== GoCastJS) ? GoCastJS : {};
+
+///
+/// \brief chat output window handler
+///        add msg's as spans with user name in bold
+///        give msg's id's
+///        animage msgs
+///
+GoCastJS.ChatUtil = function(objIn)
+{
+  this.domObj = objIn;
+  this.jqChat = $(this.domObj);
+  this.msgNumber = 0;
+}; // Item object
+
+GoCastJS.ChatUtil.prototype.addMsg = function(msg)
+{
+  var domMsg = '<span id="'+ this.msgNumber + '">    ' + msg + '<br></span>',
+      atBottom,
+      span;
+  // get scroll pos
+  //app.log(2, 'public-message scrollTop ' + jqChat.scrollTop()
+  //        + ' scrollHeight ' + jqChat[0].scrollHeight
+  //        + ' clientHeight ' + jqChat[0].clientHeight);
+  // detect scroll bar at bottom
+  // looks like the mouse wheel can put the scroll pos < 1em from bottom so fudge it
+  atBottom = Math.abs((this.jqChat.scrollTop() + this.jqChat[0].clientHeight) - this.jqChat[0].scrollHeight) < 10;
+  this.jqChat.append(domMsg); // Add message to Message Board.
+  if (atBottom) { // if we were at bottom before msg append scroll to bottom
+    this.jqChat.scrollTop(this.jqChat[0].scrollHeight);
+    //this.jqChat.animate({scrollTop : this.jqChat[0].scrollHeight},'fast');
+    // flash new msg
+    span = $("span#" + this.msgNumber, this.jqChat);
+    //span.animate({color:"red"},
+    span.animate({backgroundColor:"red"},
+      {duration: 0,
+       complete: function() {
+        //span.animate({backgroundColor:"transparent"}, 500); // doesn't work, end color is white
+        span.animate({backgroundColor:"black"}, 500);
+       }
+    });
+  } else { // flash chat border
+    this.jqChat.animate({backgroundColor:"red"},
+      {duration: 0,
+       complete: function()
+       {
+         this.jqChat.animate({backgroundColor:"black"}, 500);
+         //this.jqChat.animate({backgroundColor:"transparent"}, 500); // doesn't work, end color is white
+       }
+    });
+  }
+  ++this.msgNumber; // increment next msg number
+}; // addMsg
