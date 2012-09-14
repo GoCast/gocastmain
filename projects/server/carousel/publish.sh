c4b155ce7d8e1ca0bada16066c4b3ba70f181ef7
Robert Wolff <rwolff@gocast.it>
2012-09-14 12:13:31 -0700

    Allow for '-d' in publish which will NOT obfuscate/minimize.

--- projects/server/carousel/publish.sh
+++ projects/server/carousel/publish.sh
@@ -3,6 +3,9 @@
 # Assume we'll be wanting confirmations asked
 confirm=1
 
+# Assume we will want minimizing and obfuscation
+devmode=0
+
 # set dest dir
 dir="carousel"
 # username@servername:  (dont forget the : at the end)
@@ -11,8 +14,10 @@ server="ec2-user@video.gocast.it:"
 
 #
 # Process command line args.
-# 
+#
 # If we have a '-y' or '-f', we will not ask permission
+# If we have '-d', we will no use the minimizer and no jscrambler either. Just push.
+#
 # Otherwise, we make the destination directory equal to the argument.
 #
 # example: ./publish.sh -y html/jk
@@ -24,6 +29,11 @@ case "$1" in
   -y|-f)
       echo ==== No confirmations will be requested.
       confirm=0
+      sleep 1
+      ;;
+  -d|--debug|-dev|--devel)
+      echo ==== No minimization. No obfuscation. DEV_PUBLISH
+      devmode=1
       sleep 1
       ;;
   * ) dir=$1
@@ -87,66 +97,79 @@ cp ../../gocastjs/webrtc/peerconnection.js $tempdest/js
 cp ../../gocastjs/ui/*.js $tempdest/js
 cp ../../gocastjs/ibb/ibb.js $tempdest/js
 
+function obfuscate() {
 # Now obfuscate and minimize files that need it.
-tempjs=$tempdest/jscramble_staging
-mkdir $tempjs
-# make a temp spot for all the files that will live in js/ in the finaldest
-mkdir $tempjs/js
-cp $tempdest/js/callcast.js $tempjs/js
-cp $tempdest/js/peerconnection.js $tempjs/js
-cp $tempdest/js/wb.js $tempjs/js
-cp $tempdest/js/ibb.js $tempjs/js
+  tempjs=$tempdest/jscramble_staging
+  mkdir $tempjs
+  # make a temp spot for all the files that will live in js/ in the finaldest
+  mkdir $tempjs/js
+  cp $tempdest/js/callcast.js $tempjs/js
+  cp $tempdest/js/peerconnection.js $tempjs/js
+  cp $tempdest/js/wb.js $tempjs/js
+  cp $tempdest/js/ibb.js $tempjs/js
 
-cw=`pwd`
-cd $tempjs
-zip -r $tempdest/jscramble_upload.zip .
-cd $cw
+  cw=`pwd`
+  cd $tempjs
+  zip -r $tempdest/jscramble_upload.zip .
+  cd $cw
 
-id=`$jspost $tempdest/jscramble_upload.zip`
-echo ==== Job submitted to JScrambler. ID is $id
-rm $tempdest/jscramble_upload.zip
+  id=`$jspost $tempdest/jscramble_upload.zip`
+  echo ==== Job submitted to JScrambler. ID is $id
+  rm $tempdest/jscramble_upload.zip
 
-# Now wait for responses
+  # Now wait for responses
 
-iswaiting=true
+  iswaiting=true
 
-# until we get a response...keep polling status
-while [ -z "$res" ]
-do
-echo ==== Checking on completion of job....
-res=`$jsstatus $id`
-sleep 2
-done
+  # until we get a response...keep polling status
+  while [ -z "$res" ]
+  do
+  echo ==== Checking on completion of job....
+  res=`$jsstatus $id`
+  sleep 2
+  done
 
-if [ $res -ne 0 ]
-then
-  echo We have encountered and ERROR. The Error code is $res.
-  exit
-fi
+  if [ $res -ne 0 ]
+  then
+    echo We have encountered and ERROR. The Error code is $res.
+    exit
+  fi
 
-echo ==== JScrambler job completed. Getting results.
+  echo ==== JScrambler job completed. Getting results.
 
-# download output in a .zip
-mkdir $tempjs/out_staging
-$jsget $id >$tempjs/out_staging/output.zip
-cw=`pwd`
-cd $tempjs/out_staging
-unzip output.zip
-# For Mac, all the files wind up in the internet quarantine - remove that issue.
-xattr -d -r com.apple.quarantine .
-cd $cw
+  # download output in a .zip
+  mkdir $tempjs/out_staging
+  $jsget $id >$tempjs/out_staging/output.zip
+  cw=`pwd`
+  cd $tempjs/out_staging
+  unzip output.zip
+  # For Mac, all the files wind up in the internet quarantine - remove that issue.
+  xattr -d -r com.apple.quarantine .
+  cd $cw
 
-# finally copy the files into their tempdest
-rm -f $tempjs/out_staging/output.zip
-cp -r $tempjs/out_staging/* $tempdest
-# finally we can remove the temporary jscrambler area before copying
-rm -rf $tempjs
+  # finally copy the files into their tempdest
+  rm -f $tempjs/out_staging/output.zip
+  cp -r $tempjs/out_staging/* $tempdest
+  # finally we can remove the temporary jscrambler area before copying
+  rm -rf $tempjs
 
+return 0
+}
+
+function minimize() {
 # minimize first using google closure compiler
-gcpublish "js/carousel.js" "js/carousel.js"
-gcpublish "js/index.js" "js/index.js"
-gcpublish "js/callcast-api.js" "js/callcast-api.js"
-gcpublish "js/fb.js" "js/fb.js"
+  gcpublish "js/carousel.js" "js/carousel.js"
+  gcpublish "js/index.js" "js/index.js"
+  gcpublish "js/callcast-api.js" "js/callcast-api.js"
+  gcpublish "js/fb.js" "js/fb.js"
+return 0
+}
+
+if [ $devmode -eq 0 ]
+then
+  obfuscate
+  minimize
+fi
 
 if [ $confirm -eq 1 ]
 then
