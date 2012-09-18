6e639f99bafd927eab05a391356c2768a5f8b933
Robert Wolff <rwolff@gocast.it>
2012-09-17 18:23:45 -0700

    Added study.gocast.it to server. Added the 'pull' script for a client to grab all the server tree into git locally. serverBot.sh changed to NOT load the etzchayim/fuse/friends rooms or schedule.xml files.

--- projects/server/servertree/serverconfig/home/ec2-user/bin/serverBot.sh
+++ projects/server/servertree/serverconfig/home/ec2-user/bin/serverBot.sh
@@ -3,5 +3,5 @@ echo Killing current running serverBot...
 echo Current server PID is `pgrep -f "serverBot.js -main"`
 pkill -f "serverBot.js -main"
 echo Running new serverbot...
-node ~/serverBot.js -main ~/etzchayim/xml/schedules.xml ~/friends/xml/schedules.xml >>~/serverBot.log 2>&1 &
+node ~/serverBot.js -main >>~/serverBot.log 2>&1 &
 echo New server PID is `pgrep -f "serverBot.js -main"`
