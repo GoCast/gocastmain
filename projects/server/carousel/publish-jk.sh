4d99698bfd39ee5122fefe261bc32ffe5533247c
jimkoning <jimkoning@gmail.com>
2012-09-18 13:23:55 -0700

    always publish to jk with -d

--- projects/server/carousel/publish-jk.sh
+++ projects/server/carousel/publish-jk.sh
@@ -3,5 +3,5 @@
 # Note - defaults to no confirmation.
 #        also, feel free to add -d to this command line arg. It gets passed along.
 #
-sh ./publish.sh html/jk -y $1
+sh ./publish.sh html/jk -y -d
 
