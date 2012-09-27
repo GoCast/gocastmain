fc8a07287b7ce7759222fe1856ab424d6f7b5816
Robert Wolff <rwolff@gocast.it>
2012-09-27 15:57:19 -0700

    - Improved switchboard.sh command script to be more informative - userLogs.sh produces a facebook and ad-hoc userlist/log from switchboard.log

--- projects/server/servertree/serverconfig/home/ec2-user/bin/switchboard.sh
+++ projects/server/servertree/serverconfig/home/ec2-user/bin/switchboard.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+echo Killing any existing switchboards
+echo Current server PID is `pgrep -f "switchboard.js"`
 pkill -f switchboard\.js
+echo Starting switchboard server...
 node ~/switchboard.js --debugcommands >>~/switchboard.log 2>&1 &
+echo New server PID is `pgrep -f "switchboard.js"`
+echo Server started.
