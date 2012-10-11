a07d5571d438652ed101f56e98fe2118aaf6894d
Manjesh Malavalli <mmalavalli@xvdth.com>
2012-10-10 19:28:59 -0700

    Minor ui changes to settings page, and updating the settings push script to have timestamps on each script and css link.

--- projects/desktop/gocastplayer/push-gcpsettings.sh
+++ projects/desktop/gocastplayer/push-gcpsettings.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function genIndexHtml() {
+  curtime=`date +%s`
+  index_html=`cat $1/index.html`
+  index_html_ts=${index_html//GOCASTTIMESTAMP/$curtime}
+  echo "$index_html_ts" > index.html
+}
+
 echo "scp -r ../../xmpp-peerconnection/gcpsettings/* ec2-user@video.gocast.it:html/mmalavalli/$1/"
 scp -r ../../xmpp-peerconnection/gcpsettings/* ec2-user@video.gocast.it:html/mmalavalli/$1/
 
@@ -14,4 +21,14 @@ scp ../../server/carousel/js/jquery-ui.js ec2-user@video.gocast.it:html/mmalaval
 
 echo "scp ../../server/carousel/js/jquery-1.8.1.js ec2-user@video.gocast.it:html/mmalavalli/$1/"
 scp ../../server/carousel/js/jquery-1.8.1.js ec2-user@video.gocast.it:html/mmalavalli/$1/
+
+echo "Generating index.html..."
+genIndexHtml ../../xmpp-peerconnection/gcpsettings
+echo "Generating index.html... DONE"
+
+echo "Copying index.html to server..."
+scp index.html ec2-user@video.gocast.it:html/mmalavalli/$1/
+echo "Copying index.html to server... DONE"
+
+rm -f index.html
 
