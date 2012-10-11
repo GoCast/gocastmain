8e59642bc6ef03dbc09bd840d8277d55c590ebfe
Manjesh Malavalli <mmalavalli@xvdth.com>
2012-10-10 18:23:33 -0700

    Redesigned the settings page.

--- projects/desktop/gocastplayer/push-gcpsettings.sh
+++ projects/desktop/gocastplayer/push-gcpsettings.sh
@@ -9,9 +9,9 @@ scp ../../gocastjs/webrtc/peerconnection.js ec2-user@video.gocast.it:html/mmalav
 echo "scp ../../server/carousel/css/jquery-ui.css ec2-user@video.gocast.it:html/mmalavalli/$1/style/"
 scp ../../server/carousel/css/jquery-ui.css ec2-user@video.gocast.it:html/mmalavalli/$1/style/
 
-echo "scp ../../server/carousel/js/jquery-ui-1.8.22.custom.min.js ec2-user@video.gocast.it:html/mmalavalli/$1/"
-scp ../../server/carousel/js/jquery-ui-1.8.22.custom.min.js ec2-user@video.gocast.it:html/mmalavalli/$1/
+echo "scp ../../server/carousel/js/jquery-ui.js ec2-user@video.gocast.it:html/mmalavalli/$1/"
+scp ../../server/carousel/js/jquery-ui.js ec2-user@video.gocast.it:html/mmalavalli/$1/
 
-echo "scp -r ../../server/carousel/js/jquery-1.7.2.dev.js ec2-user@video.gocast.it:html/mmalavalli/$1/"
-scp ../../server/carousel/js/jquery-1.7.2.dev.js ec2-user@video.gocast.it:html/mmalavalli/$1/
+echo "scp ../../server/carousel/js/jquery-1.8.1.js ec2-user@video.gocast.it:html/mmalavalli/$1/"
+scp ../../server/carousel/js/jquery-1.8.1.js ec2-user@video.gocast.it:html/mmalavalli/$1/
 
