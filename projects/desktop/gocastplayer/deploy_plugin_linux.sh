cee776d8089d3505f46f1ff552f3ecf81431fbb8
Robert Wolff <rwolff@gocast.it>
2012-11-08 17:07:04 -0800

    Replacing the long lost script for generating the .tgz file for distribution on linux.

---
+++ projects/desktop/gocastplayer/deploy_plugin_linux.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd build
+
+mkdir -p GoCastPlayer
+cp bin/GCP/npGCP.so GoCastPlayer/npGCP_`uname -m`.so
+
+# create install.sh
+echo "Generating GoCastPlayer/install.sh"
+echo "#!/bin/bash" > GoCastPlayer/install.sh
+echo "" >> GoCastPlayer/install.sh
+echo "echo \"GoCastPlayer Install: Plugin will be installed in ~/.mozilla/plugins\"" >> GoCastPlayer/install.sh
+echo "mkdir -p ~/.mozilla" >> GoCastPlayer/install.sh
+echo "mkdir -p ~/.mozilla/plugins" >> GoCastPlayer/install.sh
+echo "cp npGCP_*.so ~/.mozilla/plugins/" >> GoCastPlayer/install.sh
+echo "echo \"GoCastPlayer Install: Done\"" >> GoCastPlayer/install.sh
+echo "" >> GoCastPlayer/install.sh
+
+# add user executable permission
+chmod u+x GoCastPlayer/install.sh
+
+#Package install.sh and npGCP_[i686|x86_64].so
+rm -f GoCastPlayer_`uname -m`.tar.gz
+tar czvf GoCastPlayer_`uname -m`.tar.gz GoCastPlayer/install.sh GoCastPlayer/npGCP_`uname -m`.so
+
+cd ..
+
