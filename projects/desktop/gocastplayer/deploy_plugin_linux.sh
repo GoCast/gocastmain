649dc998b7883b23a53c21a712a6f32f500b9537
Robert Wolff <rwolff@gocast.it>
2012-11-09 17:16:00 -0800

    Linux updates with README_linux as well.

--- projects/desktop/gocastplayer/deploy_plugin_linux.sh
+++ projects/desktop/gocastplayer/deploy_plugin_linux.sh
@@ -4,6 +4,7 @@ cd build
 
 mkdir -p GoCastPlayer
 cp bin/GCP/npGCP.so GoCastPlayer/npGCP_`uname -m`.so
+cp README_linux GoCastPlayer/README_linux
 
 # create install.sh
 echo "Generating GoCastPlayer/install.sh"
@@ -21,7 +22,7 @@ chmod u+x GoCastPlayer/install.sh
 
 #Package install.sh and npGCP_[i686|x86_64].so
 rm -f GoCastPlayer_`uname -m`.tar.gz
-tar czvf GoCastPlayer_`uname -m`.tar.gz GoCastPlayer/install.sh GoCastPlayer/npGCP_`uname -m`.so
+tar czvf GoCastPlayer_`uname -m`.tar.gz GoCastPlayer/install.sh GoCastPlayer/README_linux GoCastPlayer/npGCP_`uname -m`.so
 
 cd ..
 
