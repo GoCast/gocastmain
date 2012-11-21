9a5f0ea5b8332f42fb2440fb407251c0766aa5eb
Robert Wolff <rwolff@gocast.it>
2012-11-21 11:06:32 -0800

    Refactor of logcatcher to lead us to file-sharing.

---
+++ projects/gocastjs/nodejs/gcutil_node.js
@@ -0,0 +1,43 @@
+//
+// GoCastJS Utilities
+//
+
+/*jslint node: true, white: true */
+
+'use strict';
+
+var GoCastJS = GoCastJS || {};
+
+//
+//
+//
+// l o g D a t e - Support for date/time-stamp logging.
+//
+//
+//
+
+GoCastJS.pad = function(num, size) {
+    var s = '000000000' + num;
+    return s.substr(s.length - size);
+};
+
+GoCastJS.pad2 = function(num) { return GoCastJS.pad(num, 2); };
+
+//
+// Return Date/Time as mm-dd-yyyy hh:mm::ss
+//
+GoCastJS.logDate = function() {
+    var d = new Date();
+
+    return GoCastJS.pad2(d.getMonth() + 1) + '-' + GoCastJS.pad2(d.getDate()) + '-' + d.getFullYear() + ' ' +
+            GoCastJS.pad2(d.getHours()) + ':' + GoCastJS.pad2(d.getMinutes()) + ':' + GoCastJS.pad2(d.getSeconds());
+};
+
+GoCastJS.fileDate = function() {
+    var d = new Date();
+
+    return GoCastJS.pad2(d.getMonth() + 1) + '-' + GoCastJS.pad2(d.getDate()) + '-' + d.getFullYear() + '_' +
+            GoCastJS.pad2(d.getHours()) + GoCastJS.pad2(d.getMinutes()) + GoCastJS.pad2(d.getSeconds());
+};
+
+module.exports = GoCastJS;
