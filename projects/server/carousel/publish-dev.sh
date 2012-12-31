ac81e0689dd94e5ab90b995e8b46aef9f2aefd5a
Robert Wolff <rwolff@gocast.it>
2012-12-30 19:25:49 -0800

    Updated login bottoms-up approach.

--- projects/server/carousel/publish-dev.sh
+++ projects/server/carousel/publish-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
 # Note - defaults to no confirmation.
-#        also, feel free to add -d to this command line arg. It gets passed along.
 #
-sh ./publish.sh -s ec2-user@dev.gocast.it dev -y -d $1
+sh ./publish.sh -s ec2-user@dev.gocast.it dev/$1 -y -d
 
