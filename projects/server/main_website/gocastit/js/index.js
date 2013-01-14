37a4d969d9d1d021e663046593110ab11ed0571c
Manjesh Malavalli <manjesh@gocast.it>
2013-01-14 00:27:44 -0800

    WIP - user accounts - Progress on the main website.

--- projects/server/main_website/gocastit/js/index.js
+++ projects/server/main_website/gocastit/js/index.js
@@ -1,3 +1,11 @@
+function autoSlide($slideshow, interval) {
+	var $radiobuttons = $('input[type="radio"]', $slideshow),
+		checkedIdx = 0;
+	setInterval(function() {
+		$radiobuttons[(++checkedIdx)%($radiobuttons.length)].checked = 'checked';
+	}, interval||5000);
+}
+
 $(document).ready(function () {
-    $('#myCarousel').carousel();
+    autoSlide($('.sp-slideshow'));
 });
