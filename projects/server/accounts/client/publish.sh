dd897903f0034a9e083b1d9884f3a043e25c41bc
Manjesh Malavalli <manjesh@gocast.it>
2013-01-15 01:03:08 -0800

    WIP - user accounts - Small UI changes to main website and changed logo of logreg.html header bar.

--- projects/server/accounts/client/publish.sh
+++ projects/server/accounts/client/publish.sh
@@ -11,7 +11,6 @@ scp -r css/* $dest/css/
 scp deps/twitter-bootstrap/css/bootstrap.css $dest/css/
 scp deps/font-awesome/css/font-awesome.css $dest/css/
 scp -r deps/font-awesome/font/* $dest/css/font/
-scp -r ../../../xmpp-peerconnection/gcpsettings/style/fonts/gillsans/* $dest/css/font/
 
 # Copy images
 scp -r deps/twitter-bootstrap/img/* $dest/img/
@@ -21,5 +20,8 @@ scp -r js/* $dest/js/
 scp -r deps/jquery-plugins/* $dest/js/
 scp deps/twitter-bootstrap/js/bootstrap.js $dest/js/
 scp ../../carousel/js/jquery-1.8.1.js $dest/js/
+
+# Copy images
+scp -r ../../main_website/gocastit/img/gocastheaderlogo.png $dest/img/
 
 echo "Publishing done"
