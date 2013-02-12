d0192e1cc29ded06780ba6fb8ed7d4f98456025d
TJ Grant <tjgrant@tatewake.com>
2013-02-12 13:08:43 -0800

    Moved set header include from tProgram to OpenGL/package.h

--- projects/ios/apps/carousel/code/source/OpenGL/package.h
+++ projects/ios/apps/carousel/code/source/OpenGL/package.h
@@ -3,6 +3,8 @@
 #include <OpenGLES/ES2/gl.h>
 #include <OpenGLES/ES2/glext.h>
 
+#include <set>
+
 #include "tPixelFormat.h"
 #include "tSurface.h"
 #include "tTexture.h"
--- projects/ios/apps/carousel/code/source/OpenGL/tProgram.h
+++ projects/ios/apps/carousel/code/source/OpenGL/tProgram.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <set>
-
 class tProgram
 {
 protected:
