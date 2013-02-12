a3da0859624c9e591ea1166c89e3dd3001239bdf
TJ Grant <tjgrant@tatewake.com>
2013-02-12 14:40:47 -0800

    Removed unused members and unused constructors from CallcastEvent

--- projects/ios/apps/carousel/code/source/Testbed/Carousel/CallcastEvent.h
+++ projects/ios/apps/carousel/code/source/Testbed/Carousel/CallcastEvent.h
@@ -32,10 +32,6 @@ public:
     int32_t         mSpotID;
     std::string     mNickname;
     std::string     mRoomname;
-    std::string     mURL;
-    tColor4b        mColor;
-    tPoint2f        mPoint;
-    float           mPenSize;
 
 public:
     CallcastEvent(EventType evt, const std::string& newNick, const std::string& newRoom)
@@ -44,12 +40,6 @@ public:
     : mEvent(evt), mSpotType(newSpotType), mSpotID(newSpotID) { }
     CallcastEvent(EventType evt, const int32_t& newSpotID)
     : mEvent(evt), mSpotID(newSpotID) { }
-    CallcastEvent(EventType evt, const int32_t& newSpotID, const std::string& newURL)
-    : mEvent(evt), mSpotID(newSpotID), mURL(newURL) { }
-    CallcastEvent(EventType evt, const int32_t& newSpotID, const tPoint2f& np)
-    : mEvent(evt), mSpotID(newSpotID), mPoint(np) { }
-    CallcastEvent(EventType evt, const int32_t& newSpotID, const tColor4b& nc, const float& np)
-    : mEvent(evt), mSpotID(newSpotID), mColor(nc), mPenSize(np) { }
     CallcastEvent(EventType evt)
     : mEvent(evt) { }
 };
