86eda38bc5cab251405ff3de75dcd4163626d9b1
Robert Wolff <bob.wolff68@gmail.com>
2013-03-13 16:30:34 -0700

    Updated script.

--- projects/server/devServerScriptsPush.sh
+++ projects/server/devServerScriptsPush.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 server="dev.gocast.it"
 dest=ec2-user@$server:scripts/
-scp inviteemail.tmpl.html settings.js backupPrune.js serverBot/serverBot.js serverBot/nodeWB.js switchboard/switchboard.js logcatcher/*.js ../gocastjs/nodejs/* accounts/server/* $dest
+scp settings.js backupPrune.js serverBot/serverBot.js serverBot/nodeWB.js switchboard/switchboard.js logcatcher/*.js ../gocastjs/nodejs/* accounts/server/* $dest
--- projects/server/videoServerScriptsPush.sh
+++ projects/server/videoServerScriptsPush.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 server="video.gocast.it"
 dest=ec2-user@$server:scripts/
-scp inviteemail.tmpl.html settings.js backupPrune.js serverBot/serverBot.js serverBot/nodeWB.js switchboard/switchboard.js logcatcher/*.js ../gocastjs/nodejs/* accounts/server/* $dest
+scp settings.js backupPrune.js serverBot/serverBot.js serverBot/nodeWB.js switchboard/switchboard.js logcatcher/*.js ../gocastjs/nodejs/* accounts/server/* $dest
 
