ac1c36f0cc7b1e7ced6600e099a1781e01e440a6
Manjesh Malavalli <manjesh@gocast.it>
2013-05-31 09:42:12 -0700

    Updated class.js and variant.js

--- projects/gocastjs/class.js
+++ projects/gocastjs/class.js
@@ -1,58 +1,103 @@
 //!
-//! class.js
+//! class.js - A OOP-like class structure for JavaScript
 //!
-//! C++ class-like definitions in JavaScript
+//! Usage:  var <ClassName> = GoCastJS.Class({
+//!             privates: {[<varname>: <value>,]+},
+//!             publics: {[<varname>: <value>,]+},
+//!             statics: {[<varname/funcname>: <value/definition>,]+},
+//!             methods: {[<funcname>: <definition>,]+}
+//!             base (optional): <ClassName>
+//!         });
+//!
+//!         var <InstanceName> = new <ClassName>({[<varname>: <initval>]+});
+//!
+//! Note: Each private member 'priv' is accessed by an auto-generated
+//!       member function 'priv()' to be used as shown below:
+//!
+//!             var x = instance.priv() // returns value of private member 'priv'
+//!             instance.priv(val) // sets private member 'priv's value to val.
 //!
 
-var GoCastJS = GoCastJS || {};
+var GoCastJS;
 
-//!
-//! Usage: var <ClassName> = GoCastJS.Class(definitions)
-//!
-//! definitions = {
-//!     constructor: function <ClassName>(args) {}
-//!     statics: { (<FunctionName>: function(args) {}, <VariableName>: value)+ }
-//!     methods: { (<FunctionName>: function(args) {},)+ }
-//! }
-//!
-GoCastJS.Class = function(definition) {
-    var defn = definition || {},
-        statics = defn.statics || {},
-        methods = defn.methods || {},
-        constructor = defn.constructor || function Empty() {};
+(function(module) {
+    module.Class = module.Class || function Class(definition) {
+        var constructor = function Constructor() {
+            var privates = {},
+                base = definition.base;
 
-    if (!constructor.name) {
-        throw 'GoCastJS.Class.NoNameForConstructorException';
-    }
-    //!
-    //! Usage: <ClassName>.extend(statics);
-    //!
-    //! Use this method to add one or more static functions to the class.
-    //!
-    constructor.extend = function(statics) {
-        var prop;
-        for (prop in statics) {
-            if (statics.hasOwnProperty(prop)) {
-                this[prop] = statics[prop];
+            // Generate private member variable get/set function.
+            function getset(priv) {
+                return function(val) {
+                    if ('undefined' === typeof(val)) {
+                        return privates[priv];
+                    }
+                    privates[priv] = val;
+                };
+            }
+
+            // Call base class constructor if present.
+            delete definition.base;
+            if (base) {
+                base.call(this, definition);
+            }
+
+            // Private members of the class along with their
+            // get/set functions.
+            for (var priv in definition.privates) {
+                if (definition.privates.hasOwnProperty(priv)) {
+                    privates[priv] = definition.privates[priv];
+                    this[priv] = getset(priv).bind(this);
+                }
+            }
+
+            // Public members of the class.
+            for (var pub in definition.publics) {
+                if (definition.publics.hasOwnProperty(pub)) {
+                    this[pub] = definition.publics[pub];
+                }
+            }
+
+            // Member variable initialization
+            if (0 < arguments.length) {
+                for (var v in arguments[0]) {
+                    if (arguments[0].hasOwnProperty(v)) {
+                        if (0 > ['undefined', 'function'].indexOf(typeof(this[v]))) {
+                            this[v] = arguments[0][v];
+                        }  else if ('undefined' !== typeof(privates[v])) {
+                            privates[v] = arguments[0][v];
+                        }
+                    }
+                }
+            }
+        }, inherit = function inherit(derived, base) {
+            function fn() {
+                this.constructor = derived;
+            }
+            
+            fn.prototype = base.prototype;
+            derived.prototype = new fn();
+        };
+
+        // Inherit from base class if present.
+        if (definition.base) {
+            inherit(constructor, definition.base);
+        }
+
+        // Static members of the class.
+        for (var prop in definition.statics) {
+            if (definition.statics.hasOwnProperty(prop)) {
+                constructor[prop] = definition.statics[prop];
             }
         }
+        
+        // Member functions of the class.
+        for (var method in definition.methods) {
+            if (definition.methods.hasOwnProperty(method)) {
+                constructor.prototype[method] = definition.methods[method]; 
+            }
+        }
+        
+        return constructor;
     };
-    //!
-    //! Usage: <ClassName>.append(methods);
-    //!
-    //! Use this method to add one or more member functions to the class.
-    //!
-    constructor.append = function(methods) {
-        var method;
-        for (method in methods) {
-            if (methods.hasOwnProperty(method)) {
-                this.prototype[method] = methods[method];
-            }
-        }
-    }
-
-    constructor.extend(statics);
-    constructor.append(methods);
-
-    return constructor;
-};
+}(GoCastJS || (GoCastJS = {})));
--- projects/gocastjs/variant.js
+++ projects/gocastjs/variant.js
@@ -1,207 +1,116 @@
-//!
-//! variant.js
-//!
-//! Event-driven variables in JavaScript. 
-//!
-var GoCastJS = GoCastJS || {};
+var GoCastJS;
 
-GoCastJS.Event = (function() {
-    return GoCastJS.Class({
-        constructor: function GoCastJS_Event(args) {
-            //!
-            //! e.name: string <name of event>
-            //!
-            this.name = args.name;
-            //!
-            //! e.pub: GoCastJS.Variant <publisher of event>
-            //!
-            this.pub = args.pub;
-        }
-    });
-}());
-//!
-//! Usage: var v = new GoCastJS.Variant(args)
-//!
-//! args = {
-//!     value: <initial value of the variant>
-//!     ontouch: function() { ... } <fired when variant is inited>
-//!     onchange: function() { ... } <fired when variant value changes>
-//! };
-//!
-GoCastJS.Variant = (function() {
-    return GoCastJS.Class({
-        constructor: function GoCastJS_Variant(args) {
-            //!
-            //! v.value <current value of v>
-            //!
-            this.value = args.value || 0;
-            //!
-            //! v.ontouch <callback called when a new
-            //!           GoCastJS.Variant is inited>
-            //!
-            this.ontouch = args.ontouch || function() {};
-            //!
-            //! v.onchange <callback called when the value of
-            //!           GoCastJS.Variant has changed>
-            //!
-            this.onchange = args.onchange || function() {};
+(function(module) {
+	module.Event = module.Event || module.Class({
+		privates: {name: '', pub: null}
+	});
+}(GoCastJS || (GoCastJS = {})));
 
-            this.events = {};
-            this.touch();
-        },
-        methods: {
-            //!
-            //! Usage: v.val(newValue: <optional>)
-            //!
-            //! If newValue is given, sets v's value to newValue
-            //! and returns v to enable method chainability.
-            //!
-            //! If no argument is given, then returns the current
-            //! value of v.
-            //!
-            val: function(newvalue) {
+(function(module) {
+	module.Variant = module.Variant || module.Class({
+		privates: {
+			value: 0, ontouch: function() {},
+			onchange: function() {}, events: {}
+		},
+		methods: {
+			val: function(newvalue) {
                 if ('undefined' !== typeof(newvalue)) {
-                    if (this.value !== newvalue) {
-                        this.value = newvalue;
+                    if (this.value() !== newvalue) {
+                        this.value(newvalue);
                         this.change();
                     }
                     this.touch();
                     return this;
                 } else {
-                    return this.value;
+                    return this.value();
                 }
-            },
-            //!
-            //! Turn on custom event
-            //!
-            //! Usage: v.on(opts)
-            //!
-            //! opts = {
-            //!     evt: string <name of custom event>,
-            //!     type: string (optional) <a 'touch' or 'change' event>
-            //!     trigger: function() { ...; return true/false; }
-            //!              <function that specifies the condition
-            //!              for triggering event>,
-            //!     callback: function() {...}
-            //!               <function to be called when event is
-            //!               triggered>
-            //! }
-            //!
-            //! If opts.evt is either 'change' or 'touch', opts.trigger
-            //! is not needed.
-            //!
+			},
             on: function(opts) {
                 if (opts.evt) {
                     if ('touch' === opts.evt) {
-                        this.ontouch = opts.callback || function() {};
+                        this.ontouch(opts.callback);
                     } else if ('change' === opts.evt) {
-                        this.onchange = opts.callback || function() {};
-                    } else if (!this.events[opts.evt]) {
-                        this.events[opts.evt] = {
-                            trigger: opts.trigger || function() { return false; },
-                            type: opts.type || 'touch',
-                            callback: opts.callback || function() {},
+                        this.onchange(opts.callback);
+                    } else if (!this.events()[opts.evt]) {
+                        this.events()[opts.evt] = {
+                            trigger: opts.trigger,
+                            type: opts.type,
+                            callback: opts.callback,
                             subscribers: []
                         };
                     }
                 }
                 return this;
             },
-            //!
-            //! Usage: v.off(evt: string) <turn off a custom event>
-            //!
             off: function(evt) {
                 if (evt) {
                     if ('touch' === evt) {
-                        this.ontouch = function() {};
+                        this.ontouch(function() {});
                     } else if('change' === evt) {
-                        this.onchange = function() {};
-                    } else if (this.events[evt]) {
-                        delete this.events[evt];
+                        this.onchange(function() {});
+                    } else if (this.events()[evt]) {
+                        delete this.events()[evt];
                     }
                 }
                 return this;
             },
-            //!
-            //! Register an event subscriber
-            //!
-            //! Usage: v.regsub(opts)
-            //!
-            //! opts = {
-            //!     evt: string <name of the event to be subscribed>
-            //!     sub: object <subscriber object>
-            //! }
-            //!
-            //! opts.sub should implement a member function as follows:
-            //! opts.sub.onsubevent = function(e: GoCastJS.Event) {...}
-            //!
             regsub: function(opts) {
-                if (opts.evt && this.events[opts.evt]) {
-                    this.events[opts.evt].subscribers.push(opts.sub);
+                if (opts.evt && this.events()[opts.evt]) {
+                    this.events()[opts.evt].subscribers.push(opts.sub);
                 }
                 return this;
             },
-            //!
-            //! Deregister an event subscriber
-            //!
-            //! Usage: v.deregsub(opts)
-            //!
-            //! opts = (See regsub());
-            //!
             deregsub: function(opts) {
                 var idx = -1;
-                if (opts.evt && this.events[opts.evt]) {
-                    idx = this.events[opts.evt].subscribers.indexOf(opts.sub);
+                if (opts.evt && this.events()[opts.evt]) {
+                    idx = this.events()[opts.evt].subscribers.indexOf(opts.sub);
                     if (0 <= idx) {
-                        this.events[opts.evt].subscribers.splice(idx, 1);
+                        this.events()[opts.evt].subscribers.splice(idx, 1);
                     }
                 }
                 return this;
             },
-            touch: function() {
-                var e;
-                setTimeout(this.ontouch.bind(this), 0);
-                for (e in this.events) {
-                    if (this.events.hasOwnProperty(e) &&
-                        'touch' === this.events[e].type &&
-                        this.events[e].trigger.bind(this)()) {
+			touch: function() {
+                setTimeout(this.ontouch().bind(this), 0);
+                for (var e in this.events()) {
+                    if (this.events().hasOwnProperty(e) &&
+                        'touch' === this.events()[e].type &&
+                        this.events()[e].trigger.bind(this)()) {
                         this.emit(e);
                     }
                 }
-            },
-            change: function() {
-                var e;
-                setTimeout(this.onchange.bind(this), 0);
-                for (e in this.events) {
-                    if (this.events.hasOwnProperty(e) &&
-                        'change' === this.events[e].type &&
-                        this.events[e].trigger.bind(this)()) {
+			},
+			change: function() {
+                setTimeout(this.onchange().bind(this), 0);
+                for (var e in this.events()) {
+                    if (this.events().hasOwnProperty(e) &&
+                        'change' === this.events()[e].type &&
+                        this.events()[e].trigger.bind(this)()) {
                         this.emit(e);
                     }
                 }
-            },
+			},
             emit: function(evt) {
-                if (evt && this.events[evt]) {
-                    setTimeout(this.events[evt].callback.bind(this), 0);
+                if (evt && this.events()[evt]) {
+                    setTimeout(this.events()[evt].callback.bind(this), 0);
                     this.publish(evt);
                 }
             },
             publish: function(evt) {
-                var i, subarray = [],
-                    pubcb = function() {
-                        subarray.pop().onsubevent(new GoCastJS.Event({
-                            name: evt,
-                            pub: this
-                        }));
-                    };
+                var i, subarray = [], pubcb = function() {
+                    subarray.pop().onevent(new module.Event({
+                        name: evt,
+                        pub: this
+                    }));
+                };
 
-                if (evt && this.events[evt]) {
-                    for (i=0;  i<this.events[evt].subscribers.length; i++) {
-                        subarray.push(this.events[evt].subscribers[i]);
+                if (evt && this.events()[evt]) {
+                    for (i=0;  i<this.events()[evt].subscribers.length; i++) {
+                        subarray.push(this.events()[evt].subscribers[i]);
                         setTimeout(pubcb.bind(this), 0);
                     }
                 }
             }
-        }
-    });
-}());
+		}
+	});
+}(GoCastJS));
