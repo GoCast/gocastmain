f007e25a7eceb8519b7aec9c3a5cdc7348146a10
TJ Grant <tjgrant@tatewake.com>
2013-10-14 00:25:37 -0700

    #576 - GoCastTalk - "userList" returns a flat string rather than a JSON array for the user list.   * This is because the JSON extract routine I've written isn't robust enough just yet.

--- projects/ios/apps/AppRTCDemo/code/php/userList.php
+++ projects/ios/apps/AppRTCDemo/code/php/userList.php
@@ -7,8 +7,28 @@ function userList()
 		$json = file_get_contents("database/accounts.json");
 		$arr = json_decode($json, true);
 
+		$arr2 = array_keys($arr);
+
+		$i = 0;
+		$c = count($arr2);
+
+		$flat_list = "";
+		foreach ($arr2 as $v)
+		{
+			$i++;
+
+			if ($i < $c)
+			{
+				$flat_list = $flat_list.$v.',';
+			}
+			else
+			{
+				$flat_list = $flat_list.$v;
+			}
+		}
+
 		$result = array("status" => "success",
-						"list" => array_keys($arr));
+						"list" => $flat_list);
 	}
 	else
 	{
