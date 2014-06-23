706a5c8dffc0493d56952b8f46a9f4ef35d9cff0
TJ Grant <tjgrant@tatewake.com>
2014-06-23 06:53:22 -0700

    #637 - GoCastTalk - "Change password" action now locks on 5 bad attempts

--- projects/ios/apps/GoCastTalk/code/php/code/changePassword.php
+++ projects/ios/apps/GoCastTalk/code/php/code/changePassword.php
@@ -42,34 +42,51 @@ function setPassword($name, $newpassword)
 
 function changePassword($name, $password, $newpassword)
 {
-	$result = "";
-	$oldpassword = getPassword($name);
-
-	if (isntEmpty($name) && isntEmpty($password) && isntEmpty($newpassword))
+	if (can_attempt_dangerous_action($name, "changePassword"))
 	{
-		if ($password === $oldpassword)
+		$note = true;
+
+		$result = "";
+		$oldpassword = getPassword($name);
+
+		if (isntEmpty($name) && isntEmpty($password) && isntEmpty($newpassword))
 		{
-			if (setPassword($name, $newpassword))
+			if ($password === $oldpassword)
 			{
-				$result = array("status" => "success",
-								"message" => "Password was changed");
+				if (setPassword($name, $newpassword))
+				{
+					$result = array("status" => "success",
+									"message" => "Password was changed");
+
+					$note = false;
+				}
+				else
+				{
+					$result = array("status" => "fail",
+									"message" => "Couldn't write to login file");
+				}
 			}
 			else
 			{
 				$result = array("status" => "fail",
-								"message" => "Couldn't write to login file");
+								"message" => "Old password is incorrect");
 			}
 		}
 		else
 		{
 			$result = array("status" => "fail",
-							"message" => "Old password is incorrect");
+							"message" => "No login file found");
 		}
 	}
 	else
 	{
-		$result = array("status" => "fail",
-						"message" => "No login file found");
+		$result = array("status" => "locked",
+						"message" => "Account locked");
+	}
+
+	if ($note)
+	{
+		note_dangerous_action($name, "changePassword");
 	}
 
 	return $result;
